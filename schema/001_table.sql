3ead86cf7de319bd936a5d24ae8f430bcdc147fc
Tolben <vitek.pasek@gmail.com>
2025-10-22 20:58:05 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: kms_keys
 CREATE TABLE IF NOT EXISTS kms_keys (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: kms_keys
 ALTER TABLE kms_keys ADD CONSTRAINT fk_kms_keys_provider FOREIGN KEY (provider_id) REFERENCES kms_providers(id) ON DELETE CASCADE;

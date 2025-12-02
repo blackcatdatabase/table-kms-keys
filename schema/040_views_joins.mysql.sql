70144c185ec5a7db69336c446a7a22989bd1c8d4
Vito416 <vito416@protonmail.com>
2025-12-02 19:27:04 +0100

    Sync submodule state after YAML views + upsert/coverage fixes

--- schema/040_views_joins.mysql.sql
+++ schema/040_views_joins.mysql.sql
@@ -1,7 +1,7 @@
--- Auto-generated from joins-mysql.psd1 (map@mtime:2025-11-27T17:49:37Z)
+-- Auto-generated from joins-mysql.yaml (map@94ebe6c)
 -- engine: mysql
 -- view:   kms_keys_status_by_provider
--- KMS keys status per provider
+
 CREATE OR REPLACE ALGORITHM=TEMPTABLE SQL SECURITY INVOKER VIEW vw_kms_keys_status_by_provider AS
 SELECT
   p.provider,

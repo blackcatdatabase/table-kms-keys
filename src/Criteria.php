d4b8f00c5d88dad0412e49293cd92d80a914bb3e
Vito416 <vito416@protonmail.com>
2025-12-04 23:04:56 +0100

    Regenerate PHP from schema

--- src/Criteria.php
+++ src/Criteria.php
@@ -38,7 +38,7 @@ final class Criteria extends BaseCriteria
     /** Columns used for full-text LIKE/ILIKE searches. */
     protected function searchable(): array
     {
-        return [ 'external_key_ref', 'algorithm' ];
+        return [ 'external_key_ref', 'purpose', 'algorithm', 'status' ];
     }
 
 /** Columns allowed in ORDER BY (falls back to filterable() when empty). */
